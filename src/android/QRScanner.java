d6c11706e27dc7009363d710da1ce6f508d380b5
Mohit <m@guupta.com>
2026-07-09 10:01:13 +0530

    Update QRScanner.java

--- src/android/QRScanner.java
+++ src/android/QRScanner.java
@@ -457,6 +457,7 @@ public class QRScanner extends CordovaPlugin implements BarcodeCallback {
                 ArrayList<BarcodeFormat> formatList = new ArrayList<BarcodeFormat>();
                 formatList.add(BarcodeFormat.QR_CODE);
                 formatList.add(BarcodeFormat.CODE_128);
+                formatList.add(BarcodeFormat.CODE_39);
                 formatList.add(BarcodeFormat.EAN_13);
                 mBarcodeView.setDecoderFactory(new DefaultDecoderFactory(formatList));
 
